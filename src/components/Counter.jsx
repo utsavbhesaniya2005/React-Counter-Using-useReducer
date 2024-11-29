071849c54c8d2106a5ba59401ded76c5695425f7
Utsav Bhesnaiya <ukbhesaniya01@gmail.com>
2024-11-29 11:22:18 +0530

    Update Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,7 @@
 import { useReducer } from "react"
 import CounterReducer from "../helper/CounterReducer";
 
-const initialValue = {
+const initialState = {
     count : 1
 }
 
@@ -10,7 +10,7 @@ const Counter = () => {
     // useReducer is a global state management techniques and used in small projects and if we have big projects we use Redux.js
     // useReducer Can do a component a free and give all its work to reducer, it can only sends action to reducer so it can not do any work instead of these it can sends action to the reducer
     // dispatch is method/Function which can sends action type to our useReducer
-    const [rec, dispatch] = useReducer(CounterReducer, initialValue);
+    const [rec, dispatch] = useReducer(CounterReducer, initialState);
 
     const handleInc = () => {
         dispatch({type:"INC"})
